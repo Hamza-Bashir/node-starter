27a061b16e725fd55ea4faa934d9f59fdf48f084
Hamza <hmza@gmail.com>
2025-05-02 18:36:44 +0500

    Something improve

--- utilis/customError.js
+++ utilis/customError.js
@@ -2,7 +2,6 @@ class CustomError extends Error{
     constructor(statusCode, message){
         super(message)
         this.statusCode = statusCode
-        this.statusCode = statusCode >= 400 && statusCode < 500 ? "fails" : "error"
     }
 }
 
